ad6668655c6cefa8c773e3ec291b436183c89d2f
urmommagey <66855793+urmommagey@users.noreply.github.com>
2021-02-15 10:38:22 -0600

    Create RDP.sh

---
+++ RDP.sh
@@ -0,0 +1,38 @@
+sudo useradd -m ALOK
+sudo adduser ALOK sudo
+echo 'ALOK:2520' | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+sudo apt-get update
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive \
+apt install --assume-yes xfce4 desktop-base
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+sudo apt install --assume-yes xscreensaver
+sudo systemctl disable lightdm.service
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo apt install nautilus nano -y 
+sudo adduser ALOK chrome-remote-desktop
+} &> /dev/null &&
+printf "\nSetup Completed " >&2 ||
+printf "\nError Occured " >&2
+printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
+read -p "Paste Here: " CRP
+su - ALOK -c """$CRP"""
+printf 'Check https://remotedesktop.google.com/access/ \n'
+printf 'Your SUDO Pasword Is 2520 \n\n\n'
+printf 'Upgrading... \n\n'
+apt -y install obs-studio
+apt -y install firefox
+wget https://media.codeweavers.com/pub/crossover/cxlinux/demo/crossover_20.0.2-1.deb
+sudo dpkg -i crossover_20.0.2-1.deb
+sudo apt install --assume-yes --fix-broken
+if sudo apt-get upgrade &> /dev/null
+then
+    printf "\n\nUpgrade Completed " >&2
+else
+    printf "\n\nError Occured " >&2
+fi
